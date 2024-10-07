d3c43951ab4c23ac337e5ee042b5c549f025c9fa
Daniel de Moura <nazuyamikima@gmail.com>
2024-10-07 16:18:28 -0300

    refactor: create input radio component

--- src/components/InputRadio.jsx
+++ src/components/InputRadio.jsx
@@ -1,14 +1,4 @@
-export const InputRadio = ({ children, avatar, value, setSex }) => {
-
-  function handleChange(event) {
-    if(event.target.value === 'M') {
-      setSex('M');
-      return
-    }
-
-    setSex('F');
-  }
-
+export const InputRadio = ({ children, avatar, value }) => {
   return (
     <label className="w-[188px] h-[80px] px-[16px] py-[12px] bg-[#FDF7E7] border border-[#04269D] rounded-xl flex items-center gap-[8px] has-[:checked]:bg-[#B8C7FC] cursor-pointer">
       <div>
@@ -16,9 +6,8 @@ export const InputRadio = ({ children, avatar, value, setSex }) => {
           className="sr-only" 
           type="radio" 
           name="sex" 
-          value={value} 
           required 
-          onChange={handleChange} 
+          value={value} 
         />
         <img className="rounded-full" src={avatar} alt="" />
       </div>
